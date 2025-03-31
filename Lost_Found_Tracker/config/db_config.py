217a236bde4eb5386d47f741754ab488536a3c70
Anuj Karoddeo <authenticwork2002@gmail.com>
2025-03-31 18:36:01 +0530

    Update db_config.py

--- Lost_Found_Tracker/config/db_config.py
+++ Lost_Found_Tracker/config/db_config.py
@@ -4,4 +4,4 @@ from pymongo import MongoClient
 def get_database():
     mongo_uri = st.secrets["connections"]["MONGODB_URI"]
     client = MongoClient(mongo_uri, tls=True, tlsAllowInvalidCertificates=True)  # Ensure TLS is enabled
-    return client["lost_found_db"]
+    return client["lostfindtracker"]  # Database name should match MongoDB Atlas
