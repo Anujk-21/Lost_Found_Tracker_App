90796d0a2493ab019255b0b4b2b7c5b1dfd8a065
Anuj Karoddeo <authenticwork2002@gmail.com>
2025-03-31 18:16:57 +0530

    Update db_config.py

--- Lost_Found_Tracker/config/db_config.py
+++ Lost_Found_Tracker/config/db_config.py
@@ -2,6 +2,6 @@ import streamlit as st
 from pymongo import MongoClient
 
 def get_database():
-    mongo_uri = st.secrets["connections"]["MONGODB_URI"]
-    client = MongoClient(mongo_uri)
+    mongo_uri = st.secrets["connections"]["MONGODB_URI"]  # Fetch from Streamlit Secrets
+    client = MongoClient(mongo_uri, tls=True, tlsAllowInvalidCertificates=True)  # Ensure TLS is enabled
     return client["lost_found_db"]
